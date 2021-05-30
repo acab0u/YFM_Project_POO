8515a421b6897ff1004839f3095a0002e1136181
Ricardo Fontes <80930807+acab0u@users.noreply.github.com>
2021-05-30 19:18:14 +0100

    First Commit

---
+++ YFM/src/main/java/pt/iade/YFM/YfmApplication.java
@@ -0,0 +1,13 @@
+package pt.iade.YFM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YfmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YfmApplication.class, args);
+	}
+
+}
---
+++ YFM/src/test/java/pt/iade/YFM/YfmApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.iade.YFM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YfmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
